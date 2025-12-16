73ad0423031a1013365569d184f09d93768226b0
olayinkaadesalu-alpha <olayinkaadesalu@gmail.com>
2025-12-16 05:27:01 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,44 @@
+    const roles = [
+    "Web Developer",
+    "IT Support Specialist",
+    "Data Entry",
+    "Data Analyst"
+  ];
+
+  let roleIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
+  const typingElement = document.getElementById("typing");
+  const typingSpeed = 100;
+  const deletingSpeed = 60;
+  const pauseAfterTyping = 1500;
+
+  function typeEffect() {
+    const currentRole = roles[roleIndex];
+
+    if (!isDeleting) {
+      typingElement.textContent = currentRole.substring(0, charIndex + 1);
+      charIndex++;
+
+      if (charIndex === currentRole.length) {
+        setTimeout(() => (isDeleting = true), pauseAfterTyping);
+      }
+    } else {
+      typingElement.textContent = currentRole.substring(0, charIndex - 1);
+      charIndex--;
+
+      if (charIndex === 0) {
+        isDeleting = false;
+        roleIndex = (roleIndex + 1) % roles.length;
+      }
+    }
+
+    setTimeout(typeEffect, isDeleting ? deletingSpeed : typingSpeed);
+  }
+
+  typeEffect();
+    
+
 // JavaScript code for skill bar
     const skillBars = document.querySelectorAll('.skill-progress');
 
@@ -25,3 +66,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
